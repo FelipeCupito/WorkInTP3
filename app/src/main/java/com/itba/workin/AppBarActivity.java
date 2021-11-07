7134d9ad6ca6d1eb70700eabd7a2e04ede83b283
msambartolomeo <msambartolomeo@itba.edu.ar>
2021-11-06 21:30:50 -0300

    started app bar implementation

---
+++ app/src/main/java/com/itba/workin/AppBarActivity.java
@@ -0,0 +1,40 @@
+package com.itba.workin;
+
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AppBarActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.app_bar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.app_bar_profile) {
+            // launch profile activity
+            return true;
+        } else if (item.getItemId() == R.id.app_bar_share) {
+            // share routine
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_SEND);
+            intent.putExtra(Intent.EXTRA_TEXT, "jaja");
+            intent.setType("text/plain");
+
+            Intent shareIntent = Intent.createChooser(intent, null);
+            startActivity(shareIntent);
+            return true;
+        } else if (item.getItemId() == R.id.app_bar_close) {
+            finish();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+}
