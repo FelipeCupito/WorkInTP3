0b1531564ecca6b1fc5423bf69737b51f16f3074
msambartolomeo <67807065+msambartolomeo@users.noreply.github.com>
2021-11-11 19:54:42 -0300

    App bar (#1)

--- app/src/main/java/com/itba/workin/MainActivity.java
+++ app/src/main/java/com/itba/workin/MainActivity.java
@@ -1,38 +1,44 @@
 package com.itba.workin;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.itba.workin.databinding.ActivityMainBinding;
+import com.itba.workin.databinding.ToolbarMainBinding;
 
-public class MainActivity extends AppCompatActivity {
-
-    private ActivityMainBinding binding;
-
+public class MainActivity extends AppBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        ActivityMainBinding mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View root = mainBinding.getRoot();
+        setContentView(root);
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_my_routines, R.id.navigation_community, R.id.navigation_favorites)
-                .build();
+        ToolbarMainBinding toolbarBinding = ToolbarMainBinding.bind(root);
+        toolbarBinding.toolbar.inflateMenu(R.menu.app_bar_menu);
+        setSupportActionBar(toolbarBinding.toolbar);
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        NavigationUI.setupWithNavController(mainBinding.navView, navController);
     }
 
+    // hay que descomentarla cuando tengamos otras views
+//    @Override
+//    public boolean onPrepareOptionsMenu(Menu menu) {
+//        MenuItem shareItem = menu.findItem(R.id.app_bar_share);
+//        shareItem.setVisible(false);
+//        MenuItem closeItem = menu.findItem(R.id.app_bar_close);
+//        closeItem.setVisible(false);
+//        MenuItem ProfileItem = menu.findItem(R.id.app_bar_profile);
+//        ProfileItem.setVisible(false);
+//
+//        return super.onPrepareOptionsMenu(menu);
+//    }
 }
