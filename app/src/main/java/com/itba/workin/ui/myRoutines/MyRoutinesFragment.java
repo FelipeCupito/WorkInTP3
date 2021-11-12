e8f27b46841a4bc9e466839b1344e910d324a4ee
msambartolomeo <67807065+msambartolomeo@users.noreply.github.com>
2021-11-12 20:55:21 -0300

    Recycle (#2)
    * added recycle dummy
    
    * removed useless code

--- app/src/main/java/com/itba/workin/ui/myRoutines/MyRoutinesFragment.java
+++ app/src/main/java/com/itba/workin/ui/myRoutines/MyRoutinesFragment.java
@@ -4,36 +4,38 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
 
 import com.itba.workin.databinding.FragmentMyRoutinesBinding;
+import com.itba.workin.ui.RecycleViewAdapter;
+
+import java.util.ArrayList;
 
 public class MyRoutinesFragment extends Fragment {
 
-    private MyRoutinesViewModel MyRoutinesViewModel;
     private FragmentMyRoutinesBinding binding;
+    private final ArrayList<RecycleViewAdapter.RoutineWrapper> dataSet = new ArrayList<>();
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        MyRoutinesViewModel =
-                new ViewModelProvider(this).get(MyRoutinesViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        MyRoutinesViewModel myRoutinesViewModel = new ViewModelProvider(this).get(MyRoutinesViewModel.class);
 
         binding = FragmentMyRoutinesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textMyRoutines;
-        MyRoutinesViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        for (int i = 0; i < 50; i++) {
+            dataSet.add(new RecycleViewAdapter.RoutineWrapper(
+                    i,"Buenas soy routines " + i, "",0,0));
+        }
+
+        RecycleViewAdapter adapter = new RecycleViewAdapter(dataSet);
+
+        binding.recyclerview.setLayoutManager(new GridLayoutManager(root.getContext(), 2));
+        binding.recyclerview.setAdapter(adapter);
+
         return root;
     }
 
