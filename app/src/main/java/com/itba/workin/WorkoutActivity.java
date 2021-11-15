6540899880774966c8c6c44ab04aa689bd8b8578
jnegroITBA <jnegro@itba.edu.ar>
2021-11-15 20:29:05 -0300

    Changed from Activity to Fragment

---
+++ app/src/main/java/com/itba/workin/WorkoutActivity.java
@@ -0,0 +1,33 @@
+package com.itba.workin;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.itba.workin.databinding.ToolbarMainBinding;
+import com.itba.workin.databinding.WorkoutActivityBinding;
+import com.itba.workin.databinding.WorkoutSimpleBinding;
+
+public class WorkoutActivity extends AppBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WorkoutActivityBinding workoutActivity = WorkoutActivityBinding.inflate(getLayoutInflater());
+        View root = workoutActivity.getRoot();
+        setContentView(root);
+
+        ToolbarMainBinding toolbarBinding = ToolbarMainBinding.bind(root);
+        toolbarBinding.toolbar.inflateMenu(R.menu.app_bar_menu);
+        setSupportActionBar(toolbarBinding.toolbar);
+
+        NavHostFragment navHostFragment = (NavHostFragment)
+                getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        assert navHostFragment != null;
+        NavController navController = navHostFragment.getNavController();
+
+
+    }
+}
