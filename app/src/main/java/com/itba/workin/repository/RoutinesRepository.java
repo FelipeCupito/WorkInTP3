e909c4c974b94fd1c7513dac54a2626ed73d59d7
msambartolomeo <msambartolomeo@itba.edu.ar>
2021-11-15 21:21:52 -0300

    added repository cycle calls

--- app/src/main/java/com/itba/workin/repository/RoutinesRepository.java
+++ app/src/main/java/com/itba/workin/repository/RoutinesRepository.java
@@ -7,12 +7,17 @@ import androidx.lifecycle.LiveData;
 
 import com.itba.workin.App;
 import com.itba.workin.backend.ApiClient;
+import com.itba.workin.backend.ApiCycleService;
 import com.itba.workin.backend.ApiFavouritesService;
 import com.itba.workin.backend.ApiResponse;
 import com.itba.workin.backend.ApiRoutineService;
 import com.itba.workin.backend.ApiUserService;
+import com.itba.workin.backend.models.FullCycle;
+import com.itba.workin.backend.models.FullCycleExercise;
 import com.itba.workin.backend.models.FullRoutine;
 import com.itba.workin.backend.models.PagedList;
+import com.itba.workin.domain.MyCycle;
+import com.itba.workin.domain.MyCycleExcercise;
 import com.itba.workin.domain.MyRoutine;
 
 import java.util.List;
@@ -22,11 +27,13 @@ public class RoutinesRepository {
     private final ApiRoutineService apiRoutineService;
     private final ApiFavouritesService apiFavouriteService;
     private final ApiUserService apiUserService;
+    private final ApiCycleService apiCycleService;
 
     public RoutinesRepository(App application) {
         this.apiRoutineService = ApiClient.create(application, ApiRoutineService.class);
         this.apiFavouriteService = ApiClient.create(application, ApiFavouritesService.class);
         this.apiUserService = ApiClient.create(application, ApiUserService.class);
+        this.apiCycleService = ApiClient.create(application, ApiCycleService.class);
     }
 
     private MyRoutine mapRoutineModelToDomain(FullRoutine routine) {
@@ -43,6 +50,34 @@ public class RoutinesRepository {
             @Override
             protected LiveData<ApiResponse<PagedList<FullRoutine>>> createCall() {
                 return apiRoutineService.getRoutines(page, size);
+            }
+        }.asLiveData();
+    }
+
+    public LiveData<Resource<List<MyCycle>>> getCycles(int routineId) {
+        return new NetworkBoundResource<PagedList<FullCycle>, List<MyCycle>>(model ->
+                model.getContent().stream()
+                        .map(MyCycle::new)
+                        .collect(toList()))
+        {
+            @NonNull
+            @Override
+            protected LiveData<ApiResponse<PagedList<FullCycle>>> createCall() {
+                return apiCycleService.getCycles(routineId);
+            }
+        }.asLiveData();
+    }
+
+    public LiveData<Resource<List<MyCycleExcercise>>> getCycleExercises(int cycleId) {
+        return new NetworkBoundResource<PagedList<FullCycleExercise>, List<MyCycleExcercise>>(model ->
+                model.getContent().stream()
+                        .map(MyCycleExcercise::new)
+                        .collect(toList()))
+        {
+            @NonNull
+            @Override
+            protected LiveData<ApiResponse<PagedList<FullCycleExercise>>> createCall() {
+                return apiCycleService.getCycleExercises(cycleId);
             }
         }.asLiveData();
     }
